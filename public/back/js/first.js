8bff33fe124c686ff1ff5391f218fae232c32055
yuner <124563@qq.com>
2017-10-31 13:55:37 +0800

    一级分类完成

---
+++ public/back/js/first.js
@@ -0,0 +1,52 @@
+/**
+ * Created by yuner on 2017/10/31.
+ */
+$(function () {
+    var currentPage=1;
+    var pageSize=3;
+    //渲染及分页
+    function render() {
+        $.ajax({
+            type:'get',
+            url:'/category/queryTopCategoryPaging',
+            data:{
+                page:currentPage,
+                pageSize:pageSize
+            },
+            success:function (data) {
+                $('tbody').html(template('tpl',data));
+                $("#paginator").bootstrapPaginator({
+                    bootstrapMajorVersion: 3,//默认是2，如果是bootstrap3版本，这个参数必填
+                    currentPage: currentPage,//当前页
+                    totalPages: Math.ceil(data.total / pageSize),//总页数
+                    size: "small",//设置控件的大小，mini, small, normal,large
+                    onPageClicked: function (event, originalEvent, type, page) {
+                        //为按钮绑定点击事件 page:当前点击的按钮值
+                        currentPage = page;
+                        render();
+                    }
+                })
+            }
+        });
+    }
+    render();
+    // 添加一级分类
+    $('.add-btn').on('click',function () {
+        $('.addmod').modal('show');
+    });
+    $('.sure-btn').on('click',function () {
+        $.ajax({
+            type:'post',
+            url:'/category/addTopCategory',
+            data:$('#form').serialize(),
+            success:function (data) {
+                // console.log(data);
+                if(data.success){
+                   currentPage=1;
+                   render();
+                   $('.addmod').modal('hide');
+               }
+            }
+        });
+    });
+})
